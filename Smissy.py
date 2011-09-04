2aa80e3d8e37de0a6c596dd3b783e9d142ff09ed
Tim Horton <hortont424@gmail.com>
2011-09-03 20:55:21 -0700

    String style

--- Smissy.py
+++ Smissy.py
@@ -1,21 +1,21 @@
 # Copyright (c) 2011, Peter Hajas
 # All rights reserved.
 
-# Redistribution and use in source and binary forms, with or without modification, are permitted 
+# Redistribution and use in source and binary forms, with or without modification, are permitted
 # provided that the following conditions are met:
 
-# Redistributions of source code must retain the above copyright notice, this list of conditions and 
+# Redistributions of source code must retain the above copyright notice, this list of conditions and
 # the following disclaimer.
-# Redistributions in binary form must reproduce the above copyright notice, this list of conditions 
-# and the following disclaimer in the documentation and/or other materials provided with the 
+# Redistributions in binary form must reproduce the above copyright notice, this list of conditions
+# and the following disclaimer in the documentation and/or other materials provided with the
 # distribution.
-# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR 
-# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND 
-# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR 
-# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL 
-# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, 
-# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER 
-# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT 
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND ANY EXPRESS OR
+# IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND
+# FITNESS FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR
+# CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+# DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+# DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER
+# IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT
 # OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 
 # Smissy, a utility for visualizing logs on your phone.
@@ -101,79 +101,84 @@ lastMessageDate = datetime.datetime.fromtimestamp(0)
 filename = os.getcwd() + "/" + sys.argv[1] + ".html"
 htmlFile = open(filename, 'w')
 
-htmlFile.write("<html>\n")
-htmlFile.write("    <head>\n")
-htmlFile.write("        <style type=\"text/css\">\n")
-htmlFile.write("            body\n")
-htmlFile.write("            {\n")
-htmlFile.write("                font-family: Helvetica;\n")
-htmlFile.write("                max-width: 768px;\n")
-htmlFile.write("                margin-left: auto;\n")
-htmlFile.write("                margin-right: auto;\n")
-htmlFile.write("            }\n")
-htmlFile.write("            .message\n")
-htmlFile.write("            {\n")
-htmlFile.write("                border-radius: 15px 15px;\n")
-htmlFile.write("                background-color: #EEE;\n")
-htmlFile.write("                border: 1px solid #AAA;\n")
-htmlFile.write("                padding: 5px 15px;\n")
-htmlFile.write("                margin: 5px;\n")
-htmlFile.write("                color: #000;\n")
-htmlFile.write("                display: table-cell;\n")
-htmlFile.write("                text-shadow: -1px 1px 0px rgba(255,255,255,0.5);\n")
-htmlFile.write("                max-width: 70%;\n")
-htmlFile.write("                clear:both;\n")
-htmlFile.write("            }\n")
-htmlFile.write("            .incoming\n")
-htmlFile.write("            {\n")
-htmlFile.write("                background-image: -webkit-linear-gradient(#FFF, #CCC);\n")
-htmlFile.write("                float: left;\n")
-htmlFile.write("            }\n")
-htmlFile.write("            .outgoing\n")
-htmlFile.write("            {\n")
-htmlFile.write("                background-image: -webkit-linear-gradient(#CEA, #8B8);\n")
-htmlFile.write("                float: right;\n")
-htmlFile.write("            }\n")
-htmlFile.write("            .date\n")
-htmlFile.write("            {\n")
-htmlFile.write("                color:#777;\n")
-htmlFile.write("                text-align: center;\n")
-htmlFile.write("                clear:both;\n")
-htmlFile.write("                float: left, right;\n")
-htmlFile.write("            }\n")
-htmlFile.write("        </style>\n")
-htmlFile.write("    </head>\n")
-htmlFile.write("    <body>\n")
+htmlFile.write("""
+<html>
+    <head>
+        <style type='text/css'>
+            body
+            {
+                font-family: Helvetica;
+                max-width: 768px;
+                margin-left: auto;
+                margin-right: auto;
+            }
+            .message
+            {
+                border-radius: 15px 15px;
+                background-color: #EEE;
+                border: 1px solid #AAA;
+                padding: 5px 15px;
+                margin: 5px;
+                color: #000;
+                display: table-cell;
+                text-shadow: -1px 1px 0px rgba(255,255,255,0.5);
+                max-width: 70%;
+                clear:both;
+            }
+            .incoming
+            {
+                background-image: -webkit-linear-gradient(#FFF, #CCC);
+                float: left;
+            }
+            .outgoing
+            {
+                background-image: -webkit-linear-gradient(#CEA, #8B8);
+                float: right;
+            }
+            .date
+            {
+                color:#777;
+                text-align: center;
+                clear:both;
+                float: left, right;
+            }
+        </style>
+    </head>
+    <body>""")
 
 for message in sortedMessages:
-    
+
     if message[4] & 1:
         sender = "You"
     else:
         sender = "Them"
-    
+
     date = datetime.datetime.fromtimestamp(message[2])
 
     text = message[3]
 
     # If more than 15 minutes have passed, show the date
-    
+
     delta = date - lastMessageDate
     if delta.seconds > 600:
         dateString = date.strftime('%b %d, %Y %I:%M')
-        htmlFile.write("        <div class=\"date\">" + dateString + "</div>\n")
+        htmlFile.write("        <div class='date'>" + dateString + "</div>\n")
 
     if not text:
         text = "[MMS / Invalid Entry]"
     else:
         text = text.encode('utf-8')
-    
+
     if message[4] & 1:
-        htmlFile.write("        <div class=\"message outgoing\">" + text + "</div>\n")
+        htmlFile.write("        <div class='message outgoing'>" + text + "</div>\n")
     else:
-        htmlFile.write("        <div class=\"message incoming\">" + text + "</div>\n")
+        htmlFile.write("        <div class='message incoming'>" + text + "</div>\n")
 
     lastMessageDate = date
+
+htmlFile.write("""
+    </body>
+</html>""")
 
 # Close the file
 htmlFile.close()
